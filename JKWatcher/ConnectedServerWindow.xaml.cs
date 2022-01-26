a204aefb34332024890030a110170e7ebab2d992
Tom Arrow <davorfilms@googlemail.com>
2022-01-26 20:28:22 +0100

    Added log that displays activity. And some other stuff. This commit is before moving on to properly do the different camera angle stuff.

--- JKWatcher/ConnectedServerWindow.xaml.cs
+++ JKWatcher/ConnectedServerWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace JKWatcher
 
         private List<Client> connections = new List<Client>();
 
+        private const int maxLogLength = 10000;
+        private string logString = "Begin of Log\n";
+
         public ConnectedServerWindow(ServerInfo serverInfoA)
         {
             serverInfo = serverInfoA;
@@ -34,6 +37,52 @@ namespace JKWatcher
             this.Title = serverInfo.Address.ToString() + " (" + serverInfo.HostName + ")";
 
             createNewConnection();
+
+            logTxt.Text = logString;
+
+            this.Closed += ConnectedServerWindow_Closed;
+        }
+
+        private void ConnectedServerWindow_Closed(object sender, EventArgs e)
+        {
+            CloseDown();
+        }
+
+        ~ConnectedServerWindow() // Not sure if needed tbh
+        {
+            CloseDown();
+        }
+
+        private void CloseDown()
+        {
+            if (connections.Count == 0) return;
+
+            foreach (JKClient.JKClient connection in connections)
+            {
+                stopDemoRecord(connection);
+                disconnect(connection);
+                break;
+            }
+        }
+
+        private void addToLog(string someString)
+        {
+            lock (logString) { 
+                someString += "\n";
+                int newLogLength = logString.Length + someString.Length;
+                if (newLogLength <= maxLogLength)
+                {
+                    logString += someString;
+                } else
+                {
+                    int cutAway = newLogLength -maxLogLength;
+                    logString = logString.Substring(cutAway) + someString;
+                }
+                Dispatcher.Invoke(()=> {
+
+                    logTxt.Text = logString;
+                });
+            }
         }
 
         private async void createNewConnection()
@@ -41,13 +90,35 @@ namespace JKWatcher
             Client connection = new Client();
             connection.Name = "Padawan";
 
-            connection.Start(ExceptionCallback);
             connection.ServerCommandExecuted += ServerCommandExecuted;
+            connection.ServerInfoChanged += Connection_ServerInfoChanged;
+            connection.Start(ExceptionCallback);
             await connection.Connect(serverInfo);
 
             playerListDataGrid.ItemsSource = connection.ClientInfo;
 
+
             connections.Add(connection);
+            addToLog("New connection created.");
+        }
+
+        // Update player list
+        private void Connection_ServerInfoChanged(ServerInfo obj)
+        {
+            
+            if (connections.Count == 0) return;
+
+            foreach (JKClient.JKClient connection in connections)
+            {
+                if (connection.Status == ConnectionStatus.Active)
+                {
+                    Dispatcher.Invoke(() => {
+
+                        playerListDataGrid.ItemsSource = connection.ClientInfo;
+                    });
+                    break;
+                }
+            }
         }
 
         private void disconnect(JKClient.JKClient connection)
@@ -57,26 +128,34 @@ namespace JKWatcher
             connection.ServerCommandExecuted -= ServerCommandExecuted;
             connection.Stop();
             connection.Dispose();
+            addToLog("Disconnected.");
         }
 
-        private void startDemoRecord(JKClient.JKClient connection)
+        private async void startDemoRecord(JKClient.JKClient connection)
         {
-            string timeString = DateTime.Now.ToString("yyyy-MMMM-dd_HH-mm-ss");
+            addToLog("Initializing demo recording...");
+            string timeString = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
             string unusedDemoFilename = Helpers.GetUnusedDemoFilename(timeString + "-" + connection.ServerInfo.MapName, connection.ServerInfo.Protocol);
-            connection.Record_f(unusedDemoFilename);
+            await connection.Record_f(unusedDemoFilename);
+
+            addToLog("Demo recording started.");
         }
         private void stopDemoRecord(JKClient.JKClient connection)
         {
+            addToLog("Stopping demo recording...");
             connection.StopRecord_f();
+            addToLog("Demo recording stopped.");
         }
 
         private Task ExceptionCallback(JKClientException exception)
         {
+            addToLog(exception.ToString());
             Debug.WriteLine(exception);
             return null;
         }
         void ServerCommandExecuted(CommandEventArgs commandEventArgs)
         {
+            addToLog(commandEventArgs.Command.Argv(0)+" "+ commandEventArgs.Command.Argv(1)+" "+ commandEventArgs.Command.Argv(2)+" "+ commandEventArgs.Command.Argv(3));
             Debug.WriteLine(commandEventArgs.Command.Argv(0));
         }
 
