bbf47d5e505af0e0eccb766db07e21e9d4a1431d
Tom Arrow <davorfilms@googlemail.com>
2022-08-16 08:37:40 +0200

    Allow up to 4 connections. Only delete connection if score is 0.

--- JKWatcher/CameraOperators/OCDCameraOperator.cs
+++ JKWatcher/CameraOperators/OCDCameraOperator.cs
@@ -68,7 +68,7 @@ namespace JKWatcher.CameraOperators
         // second connection [1] follows blue flag
         private void Run(CancellationToken ct)
         {
-            
+
             while (true)
             {
                 System.Threading.Thread.Sleep(100);
@@ -87,6 +87,11 @@ namespace JKWatcher.CameraOperators
 
         List<int> lastActivePlayers = new List<int>();
 
+        public int MaxAllowedServerConnections { get; set; } = 4;
+
+        private Dictionary<int, int> lastFollowedPlayers = new Dictionary<int, int>(); // clientnum (watcher client) -> clientnum (player) dictionary. remember who we were watching last loop. to detect changes.
+        private Dictionary<int, DateTime> lastFollowedPlayerChanges = new Dictionary<int, DateTime>(); // clientnum (watcher client) -> DateTime of last change.
+
         private void MainLoopFunction()
         {
             // First, let's figure out the current count of players.
@@ -102,25 +107,65 @@ namespace JKWatcher.CameraOperators
                 if (infoPool.playerInfo[i].infoValid && infoPool.playerInfo[i].team != Team.Spectator && !myOwnConnections.Contains(i)) activePlayers.Add(i); // Don't count myself. Don't count spectators.
             }
 
-            if(activePlayers.Count() > 2 && !Enumerable.SequenceEqual(activePlayers,lastActivePlayers))
+            if(activePlayers.Count > MaxAllowedServerConnections && !Enumerable.SequenceEqual(activePlayers,lastActivePlayers))
             {
                 Console.Beep();
             }
 
-            if (activePlayers.Count() > 1 && connections.Count() < 2) getMoreConnections(1); // If more than 1 player here, get another connection
-            if(activePlayers.Count() <= 1 && connections.Count() > 1) // If only 1 player here, get rid of extra connections.
+            if (activePlayers.Count > connections.Count && connections.Count < MaxAllowedServerConnections) getMoreConnections(Math.Min(activePlayers.Count- connections.Count, MaxAllowedServerConnections-connections.Count)); // If more than 1 player here, get another connection
+            //if(activePlayers.Count() <= 1 && connections.Count() > 1) // If only 1 player here, get rid of extra connections.
+            if(activePlayers.Count < connections.Count && connections.Count > 1) // If only 1 player here, get rid of extra connections.
             {
-                for(int i = connections.Count-1; i > 0; i--)
+                int connectionsToDestroy = Math.Min(connections.Count-1,connections.Count - activePlayers.Count);
+                for (int i = connections.Count-1; i > 0; i--)
                 {
-                    destroyConnection(connections[i]); 
+                    if (connectionsToDestroy == 0) break;
+                    int currentlySpeccedPlayer = connections[i].client.playerStateClientNum;
+
+                    // Check if the player this connection follows is being followed by another connection (otherwise not really safe to destroy)
+                    int othersFollowingSamePlayer = 0;
+                    for(int b = 0; b < connections.Count; b++)
+                    {
+                        if (b != i && connections[b].client.playerStateClientNum == currentlySpeccedPlayer) othersFollowingSamePlayer++;
+                    }
+                    if (othersFollowingSamePlayer == 0) continue; // We can't destroy this one. It's the only one following this player.
+
+                    if(activePlayers.Count() == 0 || connections[i].client.playerStateClientNum == connections[i].ClientNum || (infoPool.playerInfo[currentlySpeccedPlayer].velocity.Length() < 0.0000001f && infoPool.playerInfo[currentlySpeccedPlayer].score.score == 0))
+                    {
+                        // We destroy the connection if there are no active players OR if it isn't spectating anyone anyway OR:
+                        // Explanation for the velocity part:
+                        // We have 2+ connections. We have 0 or 1 players. But we never destroy connections[0]. 
+                        // It's possible that the active player is being spectated by connections[1].
+                        // If we destroy connections[1], it will interrupt the demo until connections[0] spectates that player again.
+                        // This could interrupt a good defrag run.
+                        // So to be safe, we only destroy a connection if the player it is spectating is not moving.
+                        // Additionally, make sure the player's score is 0. The score is the time into the map. On very long maps like mountains, a player might have a velocity of 0 temporarily despite being in the middle of the run.
+                        destroyConnection(connections[i]);
+                        connectionsToDestroy--;
+                    }
                 }
+            }
+
+            // Detect changes in who we are following and remember the times a change happened.
+            foreach (Connection conn in connections)
+            {
+                int myClientNum = conn.ClientNum.GetValueOrDefault(-1);
+                int currentlySpeccedPlayer = conn.client.playerStateClientNum;
+
+                if (lastFollowedPlayers[myClientNum] != currentlySpeccedPlayer || !lastFollowedPlayerChanges.ContainsKey(myClientNum))
+                {
+                    lastFollowedPlayerChanges[myClientNum] = DateTime.Now;
+                }
+                lastFollowedPlayers[myClientNum] = currentlySpeccedPlayer;
             }
 
             List<int> currentlySpectatedPlayers = new List<int>();
             currentlySpectatedPlayers.AddRange(myOwnConnections); // This isn't strictly semantically logical but it will do the job. Avoid speccing itself.
             foreach (Connection conn in connections)
             {
+                int myClientNum = conn.ClientNum.GetValueOrDefault(-1);
                 int currentlySpeccedPlayer = conn.client.playerStateClientNum;
+
                 if (currentlySpectatedPlayers.Contains(currentlySpeccedPlayer))
                 {
                     // This guy is already being spectated. Let's spectate someone else instead.
@@ -143,6 +188,8 @@ namespace JKWatcher.CameraOperators
                     currentlySpectatedPlayers.Add(currentlySpeccedPlayer);
                 }
             }
+
+
 
             lastActivePlayers.Clear();
             lastActivePlayers.AddRange(activePlayers);
--- JKWatcher/Connection.cs
+++ JKWatcher/Connection.cs
@@ -54,6 +54,25 @@ namespace JKWatcher
         CS_MODELS=32
     }
 
+    enum PersistantEnum {
+        PERS_SCORE,                     // !!! MUST NOT CHANGE, SERVER AND GAME BOTH REFERENCE !!!
+        PERS_HITS,                      // total points damage inflicted so damage beeps can sound on change
+        PERS_RANK,                      // player rank or team rank
+        PERS_TEAM,                      // player team
+        PERS_SPAWN_COUNT,               // incremented every respawn
+        PERS_PLAYEREVENTS,              // 16 bits that can be flipped for events
+        PERS_ATTACKER,                  // clientnum of last damage inflicter
+        PERS_ATTACKEE_ARMOR,            // health/armor of last person we attacked
+        PERS_KILLED,                    // count of the number of times you died
+                                        // player awards tracking
+        PERS_IMPRESSIVE_COUNT,          // two railgun hits in a row
+        PERS_EXCELLENT_COUNT,           // two successive kills in a short amount of time
+        PERS_DEFEND_COUNT,              // defend awards
+        PERS_ASSIST_COUNT,              // assist awards
+        PERS_GAUNTLET_FRAG_COUNT,       // kills with the guantlet
+        PERS_CAPTURES                   // captures
+    }
+
     public enum RequestCategory
     {
         NONE,
@@ -758,6 +777,13 @@ namespace JKWatcher
                         infoPool.teamInfo[(int)JKClient.Team.Blue].lastFlagCarrierUpdate = DateTime.Now;
                     }
                 }
+            }
+
+            PlayerState currentPs = client.PlayerState;
+            if(currentPs.ClientNum >= 0 && currentPs.ClientNum < client.ClientHandler.MaxClients) // Dunno why it shouldnt be but i dont want any crashes.
+            {
+                // Update the followed player's score in realtime.
+                infoPool.playerInfo[currentPs.ClientNum].score.score = currentPs.Persistant[(int)PersistantEnum.PERS_SCORE];
             }
 
             // Save flag positions. 
