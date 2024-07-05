88a777c557d40ccb6a501a78327ecbe3c03b4847
Tom <tomarrowtomarrow@hotmail.com>
2024-07-06 01:36:23 +0200

    Add pointless test to glicko with weight 2.0 (doesnt work or do anything sensible)

--- Tests/GlickoTest.cs
+++ Tests/GlickoTest.cs
@@ -72,7 +72,7 @@ namespace Tests
                 ratings.Add(new Rating(calc));
             }
             Random rnd = new Random();
-            for(int i = 0; i < 20000; i++)
+            for(int i = 0; i < 10000; i++)
             {
                 int player1 = rnd.Next(0, playerSkills.Length);
                 int player2 = player1;
@@ -103,6 +103,57 @@ namespace Tests
             calc.UpdateRatings(periodResults);
 
             Trace.WriteLine($"All weighted 0.5 results: \n");
+            for (int i = 0; i < playerSkills.Length; i++)
+            {
+                Trace.WriteLine($"{i}: {ratings[i].GetRating()}+-{ratings[i].GetRatingDeviation()}");
+            }
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestWeighted2()
+        {
+            double[] playerSkills = new double[] {1,1,1,1,1,1,1,1,1,3 };
+            RatingCalculator calc = new RatingCalculator();
+            RatingPeriodResults periodResults = new RatingPeriodResults();
+
+            List<Rating> ratings = new List<Rating>();
+            foreach(double playerSkill in playerSkills)
+            {
+                ratings.Add(new Rating(calc));
+            }
+            Random rnd = new Random();
+            for(int i = 0; i < 10000; i++)
+            {
+                int player1 = rnd.Next(0, playerSkills.Length);
+                int player2 = player1;
+                while (player2 == player1)
+                {
+                    player2 = rnd.Next(0, playerSkills.Length);
+                }
+                Rating firstPlayer = ratings[player1];
+                Rating secondPlayer = ratings[player2];
+                double skill1 = playerSkills[player1];
+                double skill2 = playerSkills[player2];
+                double skillMultiplier = 1.0 / (skill1 + skill2);
+                skill1 *= skillMultiplier;
+                skill2 *= skillMultiplier;
+                double diceThrow = rnd.NextDouble();
+                if (diceThrow < skill1)
+                {
+                    periodResults.AddResult(firstPlayer,secondPlayer,2);
+                } else
+                {
+                    periodResults.AddResult(secondPlayer, firstPlayer,2);
+                }
+                if((i % 15)==0)
+                {
+                    calc.UpdateRatings(periodResults);
+                }
+            }
+            calc.UpdateRatings(periodResults);
+
+            Trace.WriteLine($"All weighted 2.0 results: \n");
             for (int i = 0; i < playerSkills.Length; i++)
             {
                 Trace.WriteLine($"{i}: {ratings[i].GetRating()}+-{ratings[i].GetRatingDeviation()}");
