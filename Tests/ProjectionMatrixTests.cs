5065516f1f53aeb30ab9bcd839fbbde697a5c636
Tom <tomarrowtomarrow@hotmail.com>
2024-07-31 17:36:56 +0200

    Put ProjectionMatrixHelper into RandomHelpers

--- Tests/ProjectionMatrixTests.cs
+++ Tests/ProjectionMatrixTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JKWatcher;
+using JKWatcher.RandomHelpers;
 using System.Numerics;
 using System.Diagnostics;
 using System.Drawing;
